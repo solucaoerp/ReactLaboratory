10c6eb542efddfb77d0a717a5b0cf7c7472cfd05
Charles Borges <solucao.erp@gmail.com>
2022-03-03 00:49:34 -0300

    GP: definição das rotas, css, criação de componentes

--- features/tests/gerenciador-projetos/src/App.js
+++ features/tests/gerenciador-projetos/src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+
+import Container from './components/layout/Container';
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <BrowserRouter>
+
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+
+      <Container customClass="min-height" >
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+        </Routes>
+      </Container>
+
+      <p>Footer</p>
+    </BrowserRouter>
   );
 }
 
