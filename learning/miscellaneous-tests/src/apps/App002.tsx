853874c70d21a72359e64d304d3e10913fabf9bd
Charles Borges <solucao.erp@gmail.com>
2023-04-16 20:58:17 -0300

    hook useState com (condicionais)

--- learning/miscellaneous-tests/src/apps/App002.tsx
+++ learning/miscellaneous-tests/src/apps/App002.tsx
@@ -4,18 +4,34 @@ import { Header } from "../components/Learning/Header";
 
 const App002 = () => {
     const [bg, setBg] = useState("#FF0000");
+    const [name, setName] = useState("Charles");
+    const [age, setAge] = useState(43);
 
-    let name = "Charles";
-    let age = 43;
+    const handle20 = () => {
+        setAge(20);
+        setBg("#00ff00");
+    }
 
-    const handleClick = () => {
+    const handle43 = () => {
+        setAge(43);
         setBg("#0000FF");
     }
 
     return (
         <div style={{ backgroundColor: bg }}>
             <Header name={name} age={age} city="Fortaleza" state="CE" />
-            <button onClick={handleClick} >Clique aqui</button>
+
+            {age === 43 && (
+                <>
+                    <button onClick={handle20} >Estou com 20 anos.</button>
+                    demais informações de 20 anos...
+                </>
+            )}
+
+            {age === 20 &&
+                <button onClick={handle43} >Estou com 43 anos.</button>
+            }
+
             <Footer />
         </div>
     )
