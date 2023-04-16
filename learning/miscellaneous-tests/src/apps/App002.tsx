53620b0e60b6c207f0fcb02a3e1f031a1a851911
Charles Borges <solucao.erp@gmail.com>
2023-04-16 20:27:34 -0300

    hook useState (compreensao)

--- learning/miscellaneous-tests/src/apps/App002.tsx
+++ learning/miscellaneous-tests/src/apps/App002.tsx
@@ -1,16 +1,19 @@
+import { useState } from "react";
 import { Footer } from "../components/Learning/Footer";
 import { Header } from "../components/Learning/Header";
 
 const App002 = () => {
+    const [bg, setBg] = useState("#FF0000");
+
     let name = "Charles";
     let age = 43;
 
     const handleClick = () => {
-        alert("handleClick");
+        setBg("#0000FF");
     }
 
     return (
-        <div>
+        <div style={{ backgroundColor: bg }}>
             <Header name={name} age={age} city="Fortaleza" state="CE" />
             <button onClick={handleClick} >Clique aqui</button>
             <Footer />
